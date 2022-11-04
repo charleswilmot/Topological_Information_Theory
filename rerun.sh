552a29fb44f52680daa8bd715e1f186042ba01f3
Charles <charles.wilmot2@hotmail.fr>
2022-11-04 14:52:29 +0100

    new set of default experiments

--- rerun.sh
+++ rerun.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm -r experiments/*
+rm -r experiments/debug
 
 rm databases/debug.sqlite
 
@@ -15,3 +15,13 @@ python -m src.scripts.configure_experiment -m \
   experiment=dimension_project \
   +experiment.ndshape_name=hypercube2,hypersphere2,hypertorus2,moebiusstrip,kleinbottle \
   ++experiment.n_repetitions=4
+
+python -m src.scripts.configure_experiment -m \
+  experiment=dimension_project2 \
+  +experiment.ndshape_name=hypercube2,hypersphere2,hypertorus2,moebiusstrip,kleinbottle \
+  ++experiment.projection_reg_coef=0.01 \
+  ++experiment.n_repetitions=4 \
+  ++experiment.n_iterations=2000 \
+  ++experiment.rotate_every=100 \
+  ++experiment.rotate_stop=500 \
+  ++experiment.log_start=100
